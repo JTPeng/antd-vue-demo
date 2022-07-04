081cfc2f73bd6abe2dfe03db99e73dee9f5b994b
JTPeng <404502753@qq.com>
2022-07-04 23:28:05 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,181 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    {{ data }}
+    <a-table
+      ref="sortTable"
+      class="sort-table"
+      :columns="columns"
+      :data-source="data"
+      :row-selection="rowSelection"
+      :expanded-row-keys.sync="expandedRowKeys"
+    />
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import Sortable from "sortablejs";
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+    width: "12%",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    width: "30%",
+    key: "address",
+  },
+];
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+const data = [
+  {
+    key: 1,
+    name: "John Brown sr.",
+    age: 60,
+    address: "New York No. 1 Lake Park",
+    children: [
+      {
+        key: 11,
+        name: "John Brown",
+        age: 42,
+        address: "New York No. 2 Lake Park",
+      },
+      {
+        key: 12,
+        name: "John Brown jr.",
+        age: 30,
+        address: "New York No. 3 Lake Park",
+        children: [
+          {
+            key: 121,
+            name: "Jimmy Brown",
+            age: 16,
+            address: "New York No. 3 Lake Park",
+          },
+        ],
+      },
+      {
+        key: 13,
+        name: "Jim Green sr.",
+        age: 72,
+        address: "London No. 1 Lake Park",
+        children: [
+          {
+            key: 131,
+            name: "Jim Green",
+            age: 42,
+            address: "London No. 2 Lake Park",
+            children: [
+              {
+                key: 1311,
+                name: "Jim Green jr.",
+                age: 25,
+                address: "London No. 3 Lake Park",
+              },
+              {
+                key: 1312,
+                name: "Jimmy Green sr.",
+                age: 18,
+                address: "London No. 4 Lake Park",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: 2,
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+  },
+];
+
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(
+      `selectedRowKeys: ${selectedRowKeys}`,
+      "selectedRows: ",
+      selectedRows
+    );
+  },
+  onSelect: (record, selected, selectedRows) => {
+    console.log(record, selected, selectedRows);
+  },
+  onSelectAll: (selected, selectedRows, changeRows) => {
+    console.log(selected, selectedRows, changeRows);
+  },
+};
+
+export default {
+  data() {
+    return {
+      data,
+      columns,
+      rowSelection,
+      expandedRowKeys: [],
+      pullIndex: "",
+    };
+  },
+  mounted() {
+    this.initSortable();
+  },
+  methods: {
+    initSortable() {
+      const el = this.$el.querySelector(".sort-table tbody");
+      const _this = this;
+      Sortable.create(el, {
+        handle: ".ant-table-row",
+        animation: 150,
+        group: { name: "name", pull: true, put: true },
+        onUpdate(evt) {
+          const oldIndex = evt.oldIndex;
+          const newIndex = evt.newIndex;
+          if (oldIndex === newIndex) return;
+          _this.sortListAndUpdate(_this.data, oldIndex, newIndex);
+          console.log("oldIndex", oldIndex);
+          console.log("newIndex", newIndex);
+        },
+        onStart(evt) {
+          console.log("onStart", evt);
+        },
+        onAdd(evt) {
+          console.log("onAdd", evt);
+        },
+        onRemove(evt) {
+          console.log("onRemove", evt);
+        },
+      });
+    },
+    sortList(list, oldIndex, newIndex) {
+      const newTableData = JSON.parse(JSON.stringify(list));
+      const data = newTableData.splice(oldIndex, 1, null);
+      newTableData.splice(
+        oldIndex < newIndex ? newIndex + 1 : newIndex,
+        0,
+        data[0]
+      );
+      newTableData.splice(oldIndex > newIndex ? oldIndex + 1 : oldIndex, 1);
+      return newTableData;
+    },
+    sortListAndUpdate(list, oldIndex, newIndex) {
+      const newTableData = this.sortList(list, oldIndex, newIndex);
+      // newTableData.forEach((item, index) => {
+      //   item.sort = index + 1;
+      // });
+      this.$nextTick(() => {
+        this.data = newTableData;
+        console.log("object", this.$refs.sortTable);
+        // this.$refs.sortTable && this.$refs.sortTable.refresh(true);
+      });
+    },
+  },
+};
+</script>
